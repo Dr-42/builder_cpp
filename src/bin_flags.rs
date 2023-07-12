3c658fe91d813d0bc2009130fbf1b264c2e64919
Spandan <royspandan1@gmail.com>
2023-07-12 15:37:57 +0530

    package includes in inc_dir vsc

--- src/bin_flags.rs
+++ src/bin_flags.rs
@@ -1,15 +1,15 @@
 use crate::builder::Target;
-use crate::utils::{log, LogLevel, TargetConfig, BuildConfig, Package};
-use std::path::Path;
+use crate::utils::{log, BuildConfig, LogLevel, Package, TargetConfig};
 use std::fs;
 use std::io::Write;
+use std::path::Path;
 use std::process::{Command, Stdio};
 
-static BUILD_DIR : &str = ".bld_cpp/bin";
+static BUILD_DIR: &str = ".bld_cpp/bin";
 #[cfg(target_os = "windows")]
-static OBJ_DIR: &str  = ".bld_cpp/obj_win32";
+static OBJ_DIR: &str = ".bld_cpp/obj_win32";
 #[cfg(target_os = "linux")]
-static OBJ_DIR: &str  = ".bld_cpp/obj_linux";
+static OBJ_DIR: &str = ".bld_cpp/obj_linux";
 
 ///Cleans the local targets
 /// # Arguments
@@ -17,12 +17,18 @@ static OBJ_DIR: &str  = ".bld_cpp/obj_linux";
 pub fn clean(targets: &Vec<TargetConfig>) {
     if Path::new(".bld_cpp").exists() {
         fs::create_dir_all(".bld_cpp").unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not remove binary directory: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not remove binary directory: {}", why),
+            );
         });
     }
     if Path::new(OBJ_DIR).exists() {
         fs::remove_dir_all(OBJ_DIR).unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not remove object directory: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not remove object directory: {}", why),
+            );
         });
         log(LogLevel::Info, &format!("Cleaning: {}", OBJ_DIR));
     }
@@ -35,7 +41,10 @@ pub fn clean(targets: &Vec<TargetConfig>) {
 
         if Path::new(&hash_path).exists() {
             fs::remove_file(&hash_path).unwrap_or_else(|why| {
-                log(LogLevel::Error, &format!("Could not remove hash file: {}", why));
+                log(
+                    LogLevel::Error,
+                    &format!("Could not remove hash file: {}", why),
+                );
             });
             log(LogLevel::Info, &format!("Cleaning: {}", &hash_path));
         }
@@ -58,11 +67,17 @@ pub fn clean(targets: &Vec<TargetConfig>) {
             }
             if Path::new(&bin_name).exists() {
                 fs::remove_file(&bin_name).unwrap_or_else(|why| {
-                    log(LogLevel::Error, &format!("Could not remove binary file: {}", why));
+                    log(
+                        LogLevel::Error,
+                        &format!("Could not remove binary file: {}", why),
+                    );
                 });
                 log(LogLevel::Log, &format!("Cleaning: {}", &bin_name));
             } else {
-                log(LogLevel::Log, &format!("Binary file does not exist: {}", &bin_name));
+                log(
+                    LogLevel::Log,
+                    &format!("Binary file does not exist: {}", &bin_name),
+                );
             }
         }
     }
@@ -75,12 +90,15 @@ pub fn clean_packages(packages: &Vec<Package>) {
     for pack in packages {
         for target in &pack.target_configs {
             #[cfg(target_os = "windows")]
-            let pack_bin_path = format!("{}/{}.dll",BUILD_DIR, &target.name);
+            let pack_bin_path = format!("{}/{}.dll", BUILD_DIR, &target.name);
             #[cfg(target_os = "linux")]
-            let pack_bin_path = format!("{}/{}.so",BUILD_DIR, &target.name);
+            let pack_bin_path = format!("{}/{}.so", BUILD_DIR, &target.name);
 
             if !Path::new(&pack_bin_path).exists() {
-                log(LogLevel::Log, &format!("Package binary does not exist: {}", &pack_bin_path));
+                log(
+                    LogLevel::Log,
+                    &format!("Package binary does not exist: {}", &pack_bin_path),
+                );
                 continue;
             }
             let cmd_str = format!("rm {}", &pack_bin_path);
@@ -91,9 +109,15 @@ pub fn clean_packages(packages: &Vec<Package>) {
                 .output()
                 .expect("failed to execute process");
             if output.status.success() {
-                log(LogLevel::Log, &format!("Cleaned package: {} of {}", &pack.name, &pack.repo));
+                log(
+                    LogLevel::Log,
+                    &format!("Cleaned package: {} of {}", &pack.name, &pack.repo),
+                );
             } else {
-                log(LogLevel::Error, &format!("Could not clean package: {} of {}", &pack.name, &pack.repo));
+                log(
+                    LogLevel::Error,
+                    &format!("Could not clean package: {} of {}", &pack.name, &pack.repo),
+                );
             }
         }
     }
@@ -104,10 +128,19 @@ pub fn clean_packages(packages: &Vec<Package>) {
 /// * `build_config` - The local build configuration
 /// * `targets` - A vector of targets to build
 /// * `gen_cc` - Whether to generate a compile_commands.json file
-pub fn build(build_config: &BuildConfig, targets: &Vec<TargetConfig>, gen_cc: bool, gen_vsc: bool, packages: &Vec<Package>) {
+pub fn build(
+    build_config: &BuildConfig,
+    targets: &Vec<TargetConfig>,
+    gen_cc: bool,
+    gen_vsc: bool,
+    packages: &Vec<Package>,
+) {
     if !Path::new("./.bld_cpp").exists() {
         fs::create_dir(".bld_cpp").unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not create bld_cpp directory: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not create bld_cpp directory: {}", why),
+            );
             std::process::exit(1);
         });
     }
@@ -121,7 +154,10 @@ pub fn build(build_config: &BuildConfig, targets: &Vec<TargetConfig>, gen_cc: bo
                 std::process::exit(1);
             });
         cc_file.write_all(b"[").unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not write to cc file: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not write to cc file: {}", why),
+            );
             std::process::exit(1);
         });
     }
@@ -132,12 +168,20 @@ pub fn build(build_config: &BuildConfig, targets: &Vec<TargetConfig>, gen_cc: bo
             .append(true)
             .open(".vscode/c_cpp_properties.json")
             .unwrap_or_else(|why| {
-                log(LogLevel::Error, &format!("Could not open vsc file: {}", why));
+                log(
+                    LogLevel::Error,
+                    &format!("Could not open vsc file: {}", why),
+                );
                 std::process::exit(1);
             });
-        
-        let inc_dirs : Vec<String> = targets.iter().map(|t| t.include_dir.clone()).collect();
-        let compiler_path : String = build_config.compiler.clone();
+
+        let mut inc_dirs: Vec<String> = targets.iter().map(|t| t.include_dir.clone()).collect();
+        for package in packages {
+            for target in &package.target_configs {
+                inc_dirs.push(target.include_dir.clone());
+            }
+        }
+        let compiler_path: String = build_config.compiler.clone();
 
         #[cfg(target_os = "windows")]
         let compiler_path = Command::new("sh")
@@ -149,14 +193,17 @@ pub fn build(build_config: &BuildConfig, targets: &Vec<TargetConfig>, gen_cc: bo
 
         #[cfg(target_os = "windows")]
         //Pick the first compiler path
-        let compiler_path = String::from_utf8(compiler_path).unwrap()
-            .split("\n").collect::<Vec<&str>>()[0].to_string()
+        let compiler_path = String::from_utf8(compiler_path)
+            .unwrap()
+            .split("\n")
+            .collect::<Vec<&str>>()[0]
+            .to_string()
             .replace("\r", "")
             .replace("\\", "/");
 
         #[cfg(target_os = "windows")]
         let vsc_json = format!(
-r#"{{
+            r#"{{
     "configurations": [
         {{
             "name": "Win32",
@@ -193,7 +240,7 @@ r#"{{
 
         #[cfg(target_os = "linux")]
         let vsc_json = format!(
-r#"{{
+            r#"{{
     "configurations": [
         {{
             "name": "Linux",
@@ -218,12 +265,16 @@ r#"{{
         );
 
         //Write to file
-        vsc_file.write_all(vsc_json.as_bytes()).unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not write to vsc file: {}", why));
-            std::process::exit(1);
-        });
+        vsc_file
+            .write_all(vsc_json.as_bytes())
+            .unwrap_or_else(|why| {
+                log(
+                    LogLevel::Error,
+                    &format!("Could not write to vsc file: {}", why),
+                );
+                std::process::exit(1);
+            });
     }
-    
 
     for target in targets {
         let mut tgt = Target::new(build_config, &target, &targets, &packages);
@@ -240,7 +291,10 @@ r#"{{
                 std::process::exit(1);
             });
         cc_file.write_all(b"]").unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not write to cc file: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not write to cc file: {}", why),
+            );
             std::process::exit(1);
         });
     }
@@ -253,10 +307,19 @@ r#"{{
 /// * `exe_target` - The exe target to run
 /// * `targets` - A vector of targets
 /// * `packages` - A vector of packages
-pub fn run (bin_args: Option<Vec<&str>>, build_config: &BuildConfig, exe_target: &TargetConfig, targets: &Vec<TargetConfig>, packages: &Vec<Package>) {
+pub fn run(
+    bin_args: Option<Vec<&str>>,
+    build_config: &BuildConfig,
+    exe_target: &TargetConfig,
+    targets: &Vec<TargetConfig>,
+    packages: &Vec<Package>,
+) {
     let trgt = Target::new(build_config, exe_target, &targets, &packages);
     if !Path::new(&trgt.bin_path).exists() {
-        log(LogLevel::Error, &format!("Could not find binary: {}", &trgt.bin_path));
+        log(
+            LogLevel::Error,
+            &format!("Could not find binary: {}", &trgt.bin_path),
+        );
         std::process::exit(1);
     }
     log(LogLevel::Log, &format!("Running: {}", &trgt.bin_path));
@@ -266,7 +329,9 @@ pub fn run (bin_args: Option<Vec<&str>>, build_config: &BuildConfig, exe_target:
             cmd.arg(arg);
         }
     }
-    cmd.stdin(Stdio::inherit()).stdout(Stdio::inherit()).stderr(Stdio::inherit());
+    cmd.stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
     let output = cmd.output();
     if !output.is_err() {
         log(LogLevel::Info, &format!("  Success: {}", &trgt.bin_path));
@@ -292,7 +357,7 @@ pub fn init(project_name: &str, is_c: bool) {
         log(LogLevel::Error, &format!("{}", output.err().unwrap()));
         std::process::exit(1);
     }
-    
+
     #[cfg(target_os = "windows")]
     let config_file = project_name.to_owned() + "/config_win32.toml";
     #[cfg(target_os = "linux")]
@@ -303,13 +368,16 @@ pub fn init(project_name: &str, is_c: bool) {
         log(LogLevel::Error, "Cannot initialise project");
         std::process::exit(1);
     }
-    
+
     let mut config_file = fs::OpenOptions::new()
         .write(true)
         .create(true)
         .open(config_file)
         .unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not create config file: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not create config file: {}", why),
+            );
             std::process::exit(1);
         });
 
@@ -319,25 +387,36 @@ pub fn init(project_name: &str, is_c: bool) {
         sample_config = "[build]\ncompiler = \"gcc\"\n\n[[targets]]\nname = \"main\"\nsrc = \"./src/\"\ninclude_dir = \"./src/include/\"\ntype = \"exe\"\ncflags = \"-g -Wall\"\nlibs = \"\"\ndeps = [\"\"]\n";
     }
 
-    config_file.write_all(sample_config.as_bytes()).unwrap_or_else(|why| {
-        log(LogLevel::Error, &format!("Could not write to config file: {}", why));
-        std::process::exit(1);
-    });
-    
+    config_file
+        .write_all(sample_config.as_bytes())
+        .unwrap_or_else(|why| {
+            log(
+                LogLevel::Error,
+                &format!("Could not write to config file: {}", why),
+            );
+            std::process::exit(1);
+        });
+
     let src_dir = project_name.to_owned() + "/src";
     let include_dir = project_name.to_owned() + "/src/include";
 
     //Create src and src/include directories
     if !Path::new(&src_dir).exists() {
         fs::create_dir(&src_dir).unwrap_or_else(|why| {
-            log(LogLevel::Warn , &format!("Project name {}", project_name));
-            log(LogLevel::Error, &format!("Could not create src directory: {}", why));
+            log(LogLevel::Warn, &format!("Project name {}", project_name));
+            log(
+                LogLevel::Error,
+                &format!("Could not create src directory: {}", why),
+            );
             std::process::exit(1);
         });
     }
     if !Path::new(&include_dir).exists() {
         fs::create_dir(&include_dir).unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not create src/include directory: {}", why));
+            log(
+                LogLevel::Error,
+                &format!("Could not create src/include directory: {}", why),
+            );
             std::process::exit(1);
         });
     }
@@ -353,7 +432,10 @@ pub fn init(project_name: &str, is_c: bool) {
             .create(true)
             .open(&main_path)
             .unwrap_or_else(|why| {
-                log(LogLevel::Error, &format!("Could not create main.cpp: {}", why));
+                log(
+                    LogLevel::Error,
+                    &format!("Could not create main.cpp: {}", why),
+                );
                 std::process::exit(1);
             });
         if is_c {
@@ -376,15 +458,25 @@ pub fn init(project_name: &str, is_c: bool) {
             .create(true)
             .open(&gitignore_path)
             .unwrap_or_else(|why| {
-                log(LogLevel::Error, &format!("Could not create .gitignore: {}", why));
+                log(
+                    LogLevel::Error,
+                    &format!("Could not create .gitignore: {}", why),
+                );
                 std::process::exit(1);
             });
-        gitignore_file.write_all(b".bld_cpp\ncompile_commands.json").unwrap_or_else(|why| {
-            log(LogLevel::Error, &format!("Could not write to .gitignore: {}", why));
-            std::process::exit(1);
-        });
+        gitignore_file
+            .write_all(b".bld_cpp\ncompile_commands.json")
+            .unwrap_or_else(|why| {
+                log(
+                    LogLevel::Error,
+                    &format!("Could not write to .gitignore: {}", why),
+                );
+                std::process::exit(1);
+            });
     }
 
-
-    log(LogLevel::Log, &format!("Project {} initialised", project_name));
+    log(
+        LogLevel::Log,
+        &format!("Project {} initialised", project_name),
+    );
 }
